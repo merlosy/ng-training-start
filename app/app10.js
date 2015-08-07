ee80b4a5eb8984d00cd1ced6fd6605b780ca64d0
merlosy <jelegros@LFR004607.corp.capgemini.com>
2015-08-07 16:09:49 +0200

    ajout de bootstrap ex10+

---
+++ app/app10.js
@@ -0,0 +1,30 @@
+(function(){
+	'use strict'
+
+	var app = angular.module('myapp', []);
+
+	app.controller('MainController', ['$scope', '$log', 
+                    function($scope, $log){
+        
+        $scope.people = [
+            {   firstname: 'Gérard'     },
+            {   firstname: 'Francis'    },
+            {   firstname: 'Josette'    },
+            {   firstname: 'Euridice'   }
+        ];
+
+        $scope.addPerson = function (name) {
+            var newPerson = {   firstname: name     };
+            $scope.people.push(newPerson);
+            $scope.newFriend = '';
+        };
+
+        $scope.removePerson = function (person) {
+            var index = $scope.people.indexOf(person);
+            if (index !== -1)
+                $scope.people.splice(index, 1);
+        };
+
+    }]);
+
+})();

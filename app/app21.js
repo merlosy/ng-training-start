0c09e6edd1c539f92ca689060828c4f7c62d2fe3
merlosy <jelegros@LFR004607.corp.capgemini.com>
2015-08-21 11:44:56 +0200

    up

---
+++ app/app21.js
@@ -0,0 +1,105 @@
+(function(){
+    'use strict'
+
+    var app = angular.module('myapp', [
+        'ui.router',
+        'myapp.person',
+        'myapp.people',
+        'myapp.people.services'
+    ]);
+
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+
+        $stateProvider.state('home', {
+            url: "/",
+            views : {
+                'list' : {
+                    templateUrl: 'list21.html'
+                },
+                'details' : {
+                    templateUrl: 'person18.html'
+                }
+            }
+        });
+
+        $urlRouterProvider.otherwise('/');  
+    }]);
+
+    app.controller('MainController', ['$scope', '$log', 'People', 'PeopleService',
+                                    function($scope, $log, People, PeopleService){
+    }]);
+
+    app.controller('PersonListController', ['$scope', '$log', 'People', 'PeopleService', 'Person', '$rootScope',
+                                    function($scope, $log, People, PeopleService, Person, $rootScope){
+        
+        // Initialisation de la vue
+        $scope.people = PeopleService.query();
+
+        $scope.addPerson = function (name) {
+            var person = {firstname: name};
+            $scope.newFriend = '';
+            PeopleService.save(person, function(data){
+                $log.debug(data);
+                // on rappelle le service pour avoir la liste à jour
+                $scope.people = PeopleService.query();
+            });
+            
+        };
+
+        $scope.removePerson = function (person) {
+            PeopleService.delete({id: person.id}, function(data){
+                $log.debug(data);
+                // on rappelle le service pour avoir la liste à jour
+                $scope.people = PeopleService.query();
+            });
+
+            if ( Person.getSelected()!==undefined && Person.getSelected().id===person.id)
+                    $scope.seePerson(undefined);
+        };
+
+        $scope.toggleFavorite = function (person) {
+            person.favorite = !person.favorite;
+            PeopleService.update({id: person.id}, person, function(data){
+
+                if ( Person.getSelected()!==undefined && Person.getSelected().id===person.id)
+                    $scope.seePerson(person);
+            });
+        };
+
+        $scope.seePerson = function (person) {
+            Person.setSelected(person);
+        };
+
+        $rootScope.$on('update-select-person', function(event, person){
+            angular.forEach($scope.people, function(item, key){
+                if (person.id===item.id)    $scope.people[key] = person;
+            })
+        });
+
+    }]);
+
+    app.controller('PersonDetailsController', ['$scope', '$log', 'Person', 'PeopleService', '$rootScope',
+                                        function($scope, $log, Person, PeopleService, $rootScope){
+
+        $scope.contactMode = 'show';
+
+        Person.onChange().then(null, null, function(person){
+            $scope.person = person;
+            $scope.contactMode = 'show';
+        });
+
+        $scope.$watch('contactMode', function(newVal){
+            if (newVal==='edit') $scope.editablePerson = angular.copy($scope.person);
+        });
+
+        $scope.updateContact = function () {
+            PeopleService.update({id: $scope.editablePerson.id}, $scope.editablePerson, function(data){
+                Person.setSelected($scope.editablePerson);
+                $rootScope.$broadcast('update-select-person', $scope.editablePerson);
+                $scope.contactMode = 'show';
+            });
+        };
+
+    }]);
+
+})();
---
+++ app/person21.js
@@ -0,0 +1,61 @@
+(function(){
+	'use strict'
+
+	var app = angular.module('myapp.person', []);
+
+
+	app.service('Person', ['$q', function($q){ 
+		var self = this,
+        	deferred = $q.defer();
+
+        this.person = undefined;
+
+        this.onChange = function() {
+	        return deferred.promise;
+	    };
+
+	    this.getSelected = function() {
+	        return self.person;
+	    };
+
+	    this.setSelected = function(person) {
+	        self.person = person;
+	        deferred.notify(self.person);
+	    };
+	}]);
+	
+
+	app.directive('personTile', ['$log', function($log){
+        return {
+            restrict: 'E',
+            replace: true,
+            template: '<div class="person-container"><div class="row" >'
+            	+'<div class="col-md-12 text-center">{{person.firstname}}</div> '
+				+'<div class="col-md-12 btn-group" role="group">'
+				+'	<button type="button" class="btn btn-sm btn-default btn-simple" ng-click="eventFavorite(person)">'
+				+'		<span class="glyphicon glyphicon-heart"'
+				+'			ng-class="{\'text-red\' : person.favorite}">'
+				+'		</span>'
+				+'	</button>'
+				+'	<button class="btn btn-sm btn-primary btn-simple" type="button" ng-click="eventSee(person)"><span class="glyphicon glyphicon-eye-open"></span></buttom>'
+				+'	<button class="btn btn-sm btn-danger btn-simple" type="button" ng-click="delete(person)" aria-label="Supprimer" title="Supprimer">'
+				+'		<span class="glyphicon glyphicon-remove"></span>'
+				+'	</button>'
+				+'</div></div></div>',
+			scope : {
+				person : '=',
+				eventSee : '&onSee',
+				eventFavorite : '&onFavorite',
+				eventDelete : '&onDelete'
+			},
+			link : function (scope, element, attrs, ctrl){
+
+				scope.delete = function(person) {
+					if (confirm("Êtes-vous sûr de vouloir supprimer "+person.firstname+"?"))
+						scope.eventDelete(person);
+				};
+			}
+        };
+    }]);
+
+})();
